5f4d460c090eed8d4e149dccd84fdfb3cba82a09
Kevin <keveleven26@gmail.com>
2021-01-09 00:12:18 -0500

    started email

--- main.py
+++ main.py
@@ -19,3 +19,17 @@ def send_message(message, user_id):
     payload = {"access_token": TOKEN}
     url = "https://graph.facebook.com/v9.0/me/messages"
     send = requests.post(url, params=payload, json=request)
+#main file
+import email, smtplib, ssl
+
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+def send_email(name, email, phone_number, availibilities):
+    subject = "Registration: " + name
+    body = 'Name: ' + name + '\nE-mail: ' + email + '\n Phone Number: ' 
+    + phone_number + '\n Availabilities: ' + availibilities
+
+
